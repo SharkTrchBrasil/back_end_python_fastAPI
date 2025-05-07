2d2d95bdc374d135bbe84d8c99744da1d66bc15e
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-05-07 15:31:05 -0300

    alter email verification

--- src/api/admin/services/email_service.py
+++ src/api/admin/services/email_service.py
@@ -12,15 +12,21 @@ app = FastAPI()
 
 def send_verification_email(to_email: str, code: str) -> Dict:
     params: resend.Emails.SendParams = {
+
+
+        
         "from": "Zap Delivery <onboarding@resend.dev>",
         "to": [to_email],
-        "subject": "Código de verificação do seu e-mail",
+        "subject": "{code} é o código de confirmação do registro do seu PDVix",
         "html": f"""
             <p>Olá!</p>
-            <p>Seu código de verificação é:</p>
-            <h2 style="font-size: 32px; color: #4CAF50;">{code}</h2>
-            <p>Digite este código no app para concluir seu cadastro.</p>
+            <p><h2 style="font-size: 32px; color: #4CAF50;">{code}</h2> é o código de confirmação do registro do seu PDVix</p>
+            
+            <p>Digite o código acima na tela do PDVix para confirmar o registro e começar a gerenciar sua loja com muito mais facilidade e eficiência!</p>
             <p>Se você não solicitou esse cadastro, ignore este e-mail.</p>
+               <p>Se você não solicitou este registro, ignore este e-mail.</p>
+            <p>Atenciosamente,</p>
+            <p>Equipe PDVix</p>
         """
     }
     email: resend.Email = resend.Emails.send(params)
