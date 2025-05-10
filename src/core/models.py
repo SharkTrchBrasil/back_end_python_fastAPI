c7398b84a84b7dc5f81243b78cb398fb38e2a3f4
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-05-10 10:49:19 -0300

    upload logo image

--- src/core/models.py
+++ src/core/models.py
@@ -62,7 +62,7 @@ class User(Base, TimestampMixin):
     hashed_password: Mapped[str] = mapped_column()
     is_active: Mapped[bool] = mapped_column(default=False)
     is_email_verified: Mapped[bool] = mapped_column(default=False)
-    verification_code: Mapped[str] = mapped_column()
+    verification_code: Mapped[Optional[str]] = mapped_column(nullable=True)
 
 
 class Role(Base, TimestampMixin):
