b68bc3c2b7827bc2e5417860d246f2ff50de51fd
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-05-17 14:30:04 -0300

    15/05/2025

--- src/api/admin/schemas/full_store.py
+++ src/api/admin/schemas/full_store.py
@@ -1,4 +1,8 @@
-from src.core.models import Store, Category, Coupon, Product, StorePaymentMethods
+from src.api.admin.schemas.category import Category
+from src.api.admin.schemas.coupon import Coupon
+from src.api.admin.schemas.payment_method import StorePaymentMethods
+from src.api.admin.schemas.product import Product
+from src.api.admin.schemas.store import Store
 
 
 class FullStoreData(Store):
