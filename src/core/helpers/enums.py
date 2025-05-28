c6cb330558dee4e83877d92c99f38fd48b5c901f
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-05-28 09:09:04 -0300

    26/05/2025 init caixa

--- src/core/helpers/enums.py
+++ src/core/helpers/enums.py
@@ -3,14 +3,13 @@ from enum import Enum
 class CashMovementType(str, Enum):
     IN = "in"
     OUT = "out"
-
 class CashierTransactionType(str, Enum):
-    SALE = "sale"
-    REFUND = "refund"
-    INFLOW = "inflow"
-    OUTFLOW = "outflow"
-    WITHDRAW = "withdraw"
-    SANGRIA = "sangria"
+    SALE = "SALE"  # Alterado para maiúsculas
+    REFUND = "REFUND"  # Alterado para maiúsculas
+    INFLOW = "INFLOW"  # Alterado para maiúsculas
+    OUTFLOW = "OUTFLOW"  # Alterado para maiúsculas
+    WITHDRAW = "WITHDRAW"  # Alterado para maiúsculas
+    SANGRIA = "SANGRIA"  # Alterado para maiúsculas
 
 class PaymentMethod(str, Enum):
     CASH = "cash"
