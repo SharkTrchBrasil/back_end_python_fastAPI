139ac76bfee4ff9b18b13421d03191edfcc4cb52
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-06-03 18:19:33 -0300

    26/05/2025 init caixa

--- src/api/app/schemas/store.py
+++ src/api/app/schemas/store.py
@@ -1,10 +1,33 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, computed_field
+
+from src.core.aws import get_presigned_url
 
 
 class Store(BaseModel):
     id: int
-    name: str
+    name: Optional[str]
+    phone: Optional[str]
+    zip_code: Optional[str]
+    street: Optional[str]
+    number: Optional[str]
+    neighborhood: Optional[str]
+    complement: Optional[str]
+    reference: Optional[str]
+    city: Optional[str]
+    state: Optional[str]
+    instagram: Optional[str]
+    facebook: Optional[str]
+    tiktok: Optional[str]
+    plan_type: Optional[str]
+    file_key: Optional[str]
+    #store_url: Optional[str]
 
+    @computed_field
+    @property
+    def image_path(self) -> str:
+        return get_presigned_url(self.file_key)
     model_config = {
         "from_attributes": True
     }
