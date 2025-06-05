178237385ca30716dee0ea69a70226db2ae57f87
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-06-04 23:29:36 -0300

    26/05/2025 init caixa

--- src/api/app/schemas/variants.py
+++ src/api/app/schemas/variants.py
@@ -6,9 +6,14 @@ from src.api.admin.schemas.product_variant_product import ProductVariantProductS
 class VariantOption(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: int
+
     name: str
-    description: str | None
+    description:  str | None
     price: int
+    discount_price: int
+    max_quantity: int
+    available: bool
+    is_free: bool
 
 class Variant(BaseModel):
     model_config = ConfigDict(from_attributes=True)
