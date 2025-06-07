6968d243db828c2c3cc7174970bb808c803f12e9
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-06-07 16:52:56 -0300

    05/06/2025 sockeio

--- src/api/app/__init__.py
+++ src/api/app/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from src.api.app.routes.auth import router as auth_router
-from src.api.app.routes.products import router as products_router  # importe o router de produtos
+from src.api.app.routes.products import router as products_router
+from src.api.app.routes.customers import  router as customers_router
 
 router = APIRouter(prefix="/app")
 
 # Inclua os routers filhos no router pai
 router.include_router(auth_router)
 router.include_router(products_router)  # adicione o router de produtos aqui
+router.include_router(customers_router)
