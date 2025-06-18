907c5f235317e663718fde0e65332f2f49182118
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-06-18 14:24:24 -0300

    rating da loja

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.templating import Jinja2Templates
 
-from src.api.socketio_instance import sio
+from src.socketio_instance import sio
 
 from src.api.admin import router as admin_router
 from src.api.app import router as app_router
