40e790f7d4410fda1ea123122c93cca700a7057f
SharkTrchBrasil <csatrabalho1@gmail.com>
2025-06-22 19:25:05 -0300

    rating da loja

--- src/api/app/schemas/store_details.py
+++ src/api/app/schemas/store_details.py
@@ -4,19 +4,19 @@ from pydantic import Field
 
 from src.api.shared_schemas.payment_method import StorePaymentMethods
 from src.api.shared_schemas.rating import RatingsSummaryOut
-from src.api.shared_schemas.store import StoreBase
-from src.api.shared_schemas.store_city import StoreCityBaseSchema
+from src.api.shared_schemas.store import Store
+
 from src.api.shared_schemas.store_delivery_options import StoreDeliveryConfigBase
 from src.api.shared_schemas.store_hours import StoreHoursSchema
 
 
 
 
-class StoreDetails(StoreBase):
+class StoreDetails(Store):
     payment_methods: list[StorePaymentMethods] = []
     delivery_config: StoreDeliveryConfigBase | None = None
     hours: list[StoreHoursSchema] = []
-    cities: list[StoreCityBaseSchema] = []
+   # cities: list[StoreCityBaseSchema] = []
 
     ratingsSummary: Optional[RatingsSummaryOut] = Field(None, alias="ratingsSummary")
 
