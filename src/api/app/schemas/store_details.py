88274fce033d9772439ba4d235b9730ab7207535
Zapdelivery <csatrabalho1@gmail.com>
2025-06-29 13:22:54 -0300

    rating da loja

--- src/api/app/schemas/store_details.py
+++ src/api/app/schemas/store_details.py
@@ -5,6 +5,7 @@ from pydantic import Field
 from src.api.shared_schemas.payment_method import StorePaymentMethods
 from src.api.shared_schemas.rating import RatingsSummaryOut
 from src.api.shared_schemas.store import Store
+from src.api.shared_schemas.store_city import StoreCityBaseSchema
 
 from src.api.shared_schemas.store_delivery_options import StoreDeliveryConfigBase
 from src.api.shared_schemas.store_hours import StoreHoursSchema
