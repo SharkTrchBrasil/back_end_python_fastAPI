656470786e6885e6ba9def7f27e3fd8d178c0b3d
Zapdelivery <csatrabalho1@gmail.com>
2025-07-01 21:27:17 -0300

    rating da loja

--- src/api/admin/routes/orders.py
+++ src/api/admin/routes/orders.py
@@ -4,7 +4,7 @@ from src.api.admin.schemas.order import Order, OrderDetails
 from src.core import models
 from src.core.database import GetDBDep
 from src.core.dependencies import GetStoreDep
-
+from fastapi import HTTPException, status
 router = APIRouter(tags=["Orders"], prefix="/stores/{store_id}/orders")
 
 @router.get("", response_model=list[Order])
@@ -15,10 +15,21 @@ def get_orders(
     return db.query(models.Order).filter_by(store_id=store.id).all()
 
 
+
+
+
 @router.get("/{order_id}", response_model=OrderDetails)
 def get_order(
         db: GetDBDep,
         store: GetStoreDep,
         order_id: int,
 ):
-    return db.query(models.Order).filter_by(store_id=store.id, id=order_id).first()
+    order = db.query(models.Order).filter_by(store_id=store.id, id=order_id).first()
+
+    if not order:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Pedido não encontrado"
+        )
+
+    return order
