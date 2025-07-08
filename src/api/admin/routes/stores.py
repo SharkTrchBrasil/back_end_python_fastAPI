a68f52f5ae21cfa8539954061bcad7f8ec8ca7b8
Zapdelivery <csatrabalho1@gmail.com>
2025-07-08 08:09:27 -0300

    realtime orders admin

--- src/api/admin/routes/stores.py
+++ src/api/admin/routes/stores.py
@@ -96,6 +96,19 @@ def create_store(
     )
     db.add(totem_auth)
 
+    # Cria as configurações gerais da loja (settings)
+
+    db_store_settings = models.StoreSettings(
+        store_id=db_store.id,
+        is_delivery_active=True,
+        is_takeout_active=True,
+        is_table_service_active=True,
+        is_store_open=True,
+        auto_accept_orders=False,
+        auto_print_orders=False
+    )
+    db.add(db_store_settings)
+
     # Cria vínculo do usuário com a loja como dono
     db_role = db.query(models.Role).filter(models.Role.machine_name == "owner").first()
     db_store_access = models.StoreAccess(user=user, role=db_role, store=db_store)
