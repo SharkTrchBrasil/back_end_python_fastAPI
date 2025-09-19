c9d80fbe7f11a4943a143292820aa703c3818464
Zapdelivery <csatrabalho1@gmail.com>
2025-09-19 13:54:54 -0300

    30/08/2025 refatoração produtos e complementos parte 2

--- src/core/config.py
+++ src/core/config.py
@@ -9,8 +9,8 @@ class Config(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     REFRESH_SECRET_KEY: str
-    AWS_ACCESS_KEY: str
-    AWS_SECRET_KEY: str
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
     AWS_REGION: str
     AWS_BUCKET_NAME: str
     RESEND_API_KEY: str
